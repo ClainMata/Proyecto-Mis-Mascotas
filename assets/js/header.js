bb8ffbdba72c26961776561c0cfb8b1517517aea
Carmen-Rohr <114531891+Carmen-Rohr@users.noreply.github.com>
2023-05-29 21:04:35 -0300

    Subí la versión final del proyecto Mis Mascotas

---
+++ assets/js/header.js
@@ -0,0 +1,17 @@
+let miHeader = `
+    <div id ="logo">
+        <img src="../assets/icons/pata.png" alt="Mi Mascota">                 
+    </div> 
+    <div class="menu">
+        <nav>               
+            <ul>
+                <li><a href="../index.html">INICIO</a> </li>
+                <li><a href="../pages/nosotros.html">NOSOTROS</a></li>
+                <li><a href="../pages/consumirAPI.html">GALERIA</a> </li>
+                <li><a href="../pages/form1.html">ADOPTA</a> </li>
+            </ul>                
+        </nav>
+    </div>
+`
+
+document.querySelector("header").innerHTML=miHeader;
